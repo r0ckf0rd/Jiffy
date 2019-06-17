f7cc508ae69c6e3811a78aea490a10f822469308
Nicholas Werner <nswerner@ucdavis.edu>
2019-06-17 00:33:49 -0400

    Style for Mobile Responsiveness

--- jiffy-backend/routes/gifs.js
+++ jiffy-backend/routes/gifs.js
@@ -2,11 +2,26 @@ var express = require('express');
 var router = express.Router();
 const axios = require('axios');
 
-/* GET users listing. */
+/* GET gifs listing. */
 router.get('/', function(req, res, next) {
+  console.log('----------')
+  console.log(req.query);
+  console.log('----------')
   axios.get("https://api.giphy.com/v1/gifs/random?q=&api_key=YFKm5MrOoMTYkX5qrLelkw4VFZfhw2Om&limit=1")
     .then((response) => {
-      console.log(response);
+      res.json(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+});
+
+router.get('/:search', function (req, res, next) {
+  console.log('----------')
+  console.log(req.params);
+  console.log('----------')
+  axios.get("https://api.giphy.com/v1/gifs/random?q=&api_key=YFKm5MrOoMTYkX5qrLelkw4VFZfhw2Om&limit=1")
+    .then((response) => {
       res.json(response.data);
     })
     .catch(function (error) {
--- jiffy-backend/routes/index.js
+++ jiffy-backend/routes/index.js
@@ -3,15 +3,15 @@ var router = express.Router();
 var axios = require('axios');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  axios.get("https://api.giphy.com/v1/gifs/random?q=&api_key=YFKm5MrOoMTYkX5qrLelkw4VFZfhw2Om&limit=1")
-    .then((response) => {
-      console.log(response);
-      res.json(response.data);
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
-});
+// router.get('/', function (req, res, next) {
+//   axios.get("https://api.giphy.com/v1/gifs/random?q=&api_key=YFKm5MrOoMTYkX5qrLelkw4VFZfhw2Om&limit=1")
+//     .then((response) => {
+//       console.log(response);
+//       res.json(response.data);
+//     })
+//     .catch(function (error) {
+//       console.log(error);
+//     })
+// });
 
 module.exports = router;
