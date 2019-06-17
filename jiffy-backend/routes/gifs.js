523f2001d5fc0ff9349e6300ba6f8df8bb15a0f1
Nicholas Werner <nswerner@ucdavis.edu>
2019-06-17 01:06:20 -0400

    Add Callbacks to setState

--- jiffy-backend/routes/gifs.js
+++ jiffy-backend/routes/gifs.js
@@ -4,10 +4,8 @@ const axios = require('axios');
 
 /* GET gifs listing. */
 router.get('/', function(req, res, next) {
-  console.log('----------')
-  console.log(req.query);
-  console.log('----------')
-  axios.get("https://api.giphy.com/v1/gifs/random?q=&api_key=YFKm5MrOoMTYkX5qrLelkw4VFZfhw2Om&limit=1")
+  axios.get("https://api.giphy.com/v1/gifs/random?q=funny+dog&rating=g&api_key=YFKm5MrOoMTYkX5qrLelkw4VFZfhw2Om&limit=1")
+  // axios.get("https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YFKm5MrOoMTYkX5qrLelkw4VFZfhw2Om&limit=1")
     .then((response) => {
       res.json(response.data);
     })
@@ -16,17 +14,5 @@ router.get('/', function(req, res, next) {
     })
 });
 
-router.get('/:search', function (req, res, next) {
-  console.log('----------')
-  console.log(req.params);
-  console.log('----------')
-  axios.get("https://api.giphy.com/v1/gifs/random?q=&api_key=YFKm5MrOoMTYkX5qrLelkw4VFZfhw2Om&limit=1")
-    .then((response) => {
-      res.json(response.data);
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
-});
 
 module.exports = router;
